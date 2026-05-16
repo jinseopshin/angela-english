f0795c82fbb2444dc376ed2ed93756045ae30b35
jinseopshin <70053858+jinseopshin@users.noreply.github.com>
2026-05-16 22:54:21 +0900

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,8 @@
-import App from "./App";
+"use client";
+import dynamic from "next/dynamic";
+
+// SSR 완전 비활성화 - localStorage 기반 앱이므로 클라이언트에서만 렌더
+const App = dynamic(() => import("./App"), { ssr: false });
 
 export default function Page() {
   return <App />;
