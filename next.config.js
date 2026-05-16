d1d3910533266427879b54a0c7ea28a02f935b49
jinseopshin <70053858+jinseopshin@users.noreply.github.com>
2026-05-16 23:21:18 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // recharts 등 브라우저 전용 패키지를 서버 번들에서 제외
+  experimental: {
+    optimizePackageImports: ['recharts'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // 서버 사이드에서 recharts 완전 제외
+      config.externals = [...(config.externals || []), 'recharts'];
+    }
+    return config;
+  },
+};
+
 module.exports = nextConfig;
